15a089aafb9b81a5377641024413595ee062a54d
Daviid <david.hany129@gmail.com>
2020-04-13 05:41:17 +0200

    Add create cluster script

---
+++ createCluster.sh
@@ -0,0 +1,11 @@
+#create cluster 
+eksctl create cluster \
+    --name capstone \
+    --region us-east-1 \  
+    --nodegroup-name standard-workers \
+    --node-type t2.small \ 
+    --nodes 2 \
+    --nodes-min 1 \
+    --nodes-max 3  
+#update kubeconfig
+aws eks --region us-east-1 update-kubeconfig --name capstone 
